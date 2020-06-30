7fd608fd83e9f05eb8b516d81b50133b51f104a4
Yashaswini29 <yashaswinihn.h.n@gmail.com>
2020-06-30 10:37:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground,Base, leftWall, rightWall,paper;
+var engine, world;
+
+function setup() {
+	createCanvas(1600, 700);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	Base = new Ground(900,650,200,20);
+	
+	leftWall = new Ground(800,610,20,100);
+	
+	rightWall = new Ground(1000,610,20,100);
+	
+	ground = new Ground(800,680,1600,20);
+	paper = new Paper(100,600);
+
+	Engine.run(engine);
+	
+}
+
+
+function draw() {
+
+  background(0);
+  ground.display();
+  paper.display();
+  Base.display();
+  leftWall.display();
+  rightWall.display();
+ 
+}
+
+function keyPressed(){
+	if(keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x : 20,y : -20})
+	}
+}
+
+
+
+
+
+
+
